b738832614f514add4465890582422905a8674d4
IvanDobrovolsky <dobrovolsky94@gmail.com>
2017-09-09 00:37:47 +0300

    Added user module and global interfaces

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,3 +1,11 @@
 export class UserModel {
-
+  constructor(
+    public id: string,
+    public name: string,
+    public email: string,
+    public phone: string,
+    public website: string,
+    public address: IUserAddress,
+    public company: IUserCompany
+  ) {}
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,35 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+declare interface IGeolocation {
+  lat: number;
+  lng: number;
+}
+
+declare interface IUserAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: IGeolocation;
+}
+
+declare interface IUserCompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+declare interface IBasicUserInfo {
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+declare interface IUser extends IBasicUserInfo {
+  id: string;
+  address: IUserAddress;
+  company: IUserCompany;
+}
